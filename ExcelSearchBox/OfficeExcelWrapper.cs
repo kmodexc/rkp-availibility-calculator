de2812585957cd05371d5da3ab23bcfcc5a1428a
Marius Schlüter <m.schlueter01@stud.uni-goettingen.de>
2020-10-03 16:59:05 +0200

    moved logic to other project rkpmodels

--- ExcelSearchBox/OfficeExcelWrapper.cs
+++ ExcelSearchBox/OfficeExcelWrapper.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Office.Interop.Excel;
+using RKPModels;
 
 namespace ExcelSearchBox
 {
--- ExcelSearchBox/SearchBox.cs
+++ ExcelSearchBox/SearchBox.cs
@@ -1,9 +1,8 @@
 ﻿using ExcelSearchBox.Properties;
+using RKPModels;
 using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Runtime.Remoting.Channels;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
